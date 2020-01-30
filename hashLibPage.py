b27ad80800420e2bda2b5a727c4a19926e08ebd0
candi955 <44098788+candi955@users.noreply.github.com>
2020-01-30 04:26:10 -0700

    Using Hashlib instead; created a page.

---
+++ hashLibPage.py
@@ -0,0 +1,15 @@
+# reference: https://docs.python.org/3/library/hashlib.html
+# https://pypi.org/project/hashlib/
+
+import hashlib
+
+m = hashlib.sha256()
+print(m.update(b"Nobody inspects"))
+print(m.update(b" the spammish repetition"))
+print(m.digest())
+# b'\x03\x1e\xdd}Ae\x15\x93\xc5\xfe\\\x00o\xa5u+7\xfd\xdf\xf7\xbcN\x84:\xa6\xaf\x0c\x95\x0fK\x94\x06'
+print(m.digest_size)
+
+print(m.block_size)
+
+print(hashlib.sha224(b"Nobody inspects the spammish repetition").hexdigest())
