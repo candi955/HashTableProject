59fd03a5a21766f625445c2dab7f08e23756672b
candi955 <44098788+candi955@users.noreply.github.com>
2020-02-01 21:33:33 -0700

    Added class color and termcolor.

--- hashLibPage.py
+++ hashLibPage.py
@@ -1,3 +1,8 @@
+# A program to fulfill the project requirements for Hash Table in class MS549.
+
+# In this project I not only practice hash tables, but also the use of color within my output, using both class color
+# variable assignment, and the library 'termcolor'.
+
 # reference: https://docs.python.org/3/library/hashlib.html
 # https://pypi.org/project/hashlib/
 # https://readthedocs.org/projects/hashlib/
@@ -6,27 +11,34 @@
 # https://www.pythoncentral.io/hashing-strings-with-python/
 # Reference for uuid to add salt to hash to prevent collision:
 # https://stackoverflow.com/questions/9594125/salt-and-hash-a-password-in-python
+# https://docs.python.org/2/library/uuid.html
 
 # Other references for hash tables in python:
 # https://www.geeksforgeeks.org/python-hash-method/'
 # https://www.geeksforgeeks.org/applications-of-hashing/
+# side note from reference:  uuid4() creates a random UUID.
 
 # importing 'hashlib' to create a hash table, and 'uuid' to salt the hash and prevent collision
 import hashlib, uuid
 
+# importing termcolor to color some of the text in the program
+# reference: https://www.programcreek.com/python/example/78943/termcolor.colored
+from termcolor import colored
 
-m = hashlib.sha256()
-print(m.update(b"Nobody inspects"))
-print(m.update(b" the spammish repetition"))
-print(m.digest())
-# b'\x03\x1e\xdd}Ae\x15\x93\xc5\xfe\\\x00o\xa5u+7\xfd\xdf\xf7\xbcN\x84:\xa6\xaf\x0c\x95\x0fK\x94\x06'
-print(m.digest_size)
+start = "\033[1m"
+end = "\033[0;0m"
 
-print(m.block_size)
+data = ['Jane Doe, 03/30/1956', 'Jim Davis, 4/15/1983', 'Mindy Watkins, 8/21/1977']
 
-print(hashlib.sha224(b"Nobody inspects the spammish repetition").hexdigest())
 
-# reference: https://www.pythoncentral.io/hashing-strings-with-python/
-print("\n\nHash object:")
-hash_object = hashlib.md5(b'Hello World')
-print(hash_object.hexdigest())
+print(start, "\nData:\n", end, data)
+
+hash_table = [None] * 20
+
+
+print(colored("\nEmpty hash table:\n", "blue"))
+print(hash_table)
+
+
+
+
