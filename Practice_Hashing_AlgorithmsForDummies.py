b943a82c9ac30679c4b3e9e594ef4f10a0825f89
candi955 <44098788+candi955@users.noreply.github.com>
2020-02-01 19:11:17 -0700

    Added a practice page for the Algorithms for Dummies practice.

---
+++ Practice_Hashing_AlgorithmsForDummies.py
@@ -0,0 +1,43 @@
+#%% md
+
+# Hashing
+
+#%%
+
+data = [22, 40, 102, 105, 23, 31, 6, 5]
+hash_table = [None] * 15
+tblLen = len(hash_table)
+
+def hash_function(value, table_size):
+    return value % table_size
+
+for value in data:
+    hash_table[hash_function(value, tblLen)] = value
+
+print("\nHash table:\n",hash_table)
+
+#%%
+
+print("\nHash table function:\n",hash_table[hash_function(102, tblLen)])
+
+#%% md
+
+## Creating your own hash function
+
+#%%
+
+from hashlib import md5, sha1
+
+def hash_f(element, i, length):
+    """ Function to create many hash functions """
+    h1 = int(md5(element.encode('ascii')).hexdigest(),16)
+    h2 = int(sha1(element.encode('ascii')).hexdigest(),16)
+    return (h1 + i*h2) % length
+
+#%%
+
+print ("\nHash table CAT 1:\n",hash_f("CAT", 1, 10**5))
+
+#%%
+
+print ("\nHash table CAT 2:\n", hash_f("CAT", 2, 10**5))
